bb84df47d1530aeee08626e2e380b19573f05e85
Tejas091999 <tejaslondhe9@gmail.com>
2023-04-24 19:22:55 +0530

    Add files via upload

---
+++ Basic Code.js
@@ -0,0 +1,6 @@
+let name = prompt("What is your name:")
+let age = parseInt(prompt("What is your age:"))
+let address = prompt("Where do you live: ")
+let myInfo = []
+myInfo.push(name,age,address)
+console.log(myInfo)
