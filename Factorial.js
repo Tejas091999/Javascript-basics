7f00872512cefd963e45b07710a81adbfcb4df83
Tejas091999 <tejaslondhe9@gmail.com>
2023-07-09 19:34:07 +0530

    Add files via upload

---
+++ Factorial.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const number = 5;
+const result = factorial(number);
+console.log(`The factorial of ${number} is ${result}.`);
