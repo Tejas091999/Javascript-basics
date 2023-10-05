d942bf5a2b11c3b491b83a5fc9900cefd823a481
Tejas091999 <tejaslondhe9@gmail.com>
2023-10-05 23:46:12 +0530

    Update Basic Code.js

--- Basic Code.js
+++ Basic Code.js
@@ -3,4 +3,4 @@ let age = parseInt(prompt("What is your age:"))
 let address = prompt("Where do you live: ")
 let myInfo = []
 myInfo.push(name,age,address)
-console.log(myInfo)
+console.log(myInfo) 
